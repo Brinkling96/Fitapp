d075bfda2f09c6ac9c9a6499a0a87f6363782336
[Sean Brady] <[sdbrady96@gmail.com]>
2022-02-21 15:09:58 -0500

    Full test script beta

--- setup_dev.sh
+++ setup_dev.sh
@@ -11,3 +11,15 @@ docker-compose up -d
 docker build -t $image  ./app 
 
 docker run -dt --name=$container --network=$net --mount src="$(pwd)"/app,target=/usr/src/app,type=bind $image
+
+docker exec -ti $container bash
+
+mkdir build
+
+cd build
+
+cmake -S .. -B .
+
+make
+
+./Tests
