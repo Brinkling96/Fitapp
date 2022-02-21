703d7746555997b993a3f28711a212780e0a5a33
[Sean Brady] <[sdbrady96@gmail.com]>
2022-02-21 13:20:19 -0500

    fixed DockerFile and Setup

--- setup_dev.sh
+++ setup_dev.sh
@@ -4,6 +4,6 @@ docker network create -d bridge FitAppNet
 
 docker-compose up -d
 
-docker build -t fitapp_cppdev:1.1 --network=FitAppNet ./app 
+docker build -t fitapp_cppdev:1.1  ./app 
 
-docker run -dt --name=fipapp_CPPDEV --mount src="$(pwd)"/app,target=/usr/src/app,type=bind fitapp_cppdev:1.1
+docker run -dt --name=fitapp_CPPDEV --network=FitAppNet --mount src="$(pwd)"/app,target=/usr/src/app,type=bind fitapp_cppdev:1.1
